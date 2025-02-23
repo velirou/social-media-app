9ddb33075cb800309c42c5cc0483471adbbe5d81
Velisarios Roussos <velirou@gmail.com>
2025-02-24 00:39:13 +0200

    fixed frontend errors

--- social-media-frontend/src/services/api.tsx
+++ social-media-frontend/src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8083', // API Gateway URL
+  baseURL: 'http://localhost:8000', // API Gateway URL
 });
 
 export default api;
